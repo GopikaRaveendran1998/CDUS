027d98f55dc25cad3cdd74e432e8bf70c69f9e22
GopikaRaveendran1998 <112853595+GopikaRaveendran1998@users.noreply.github.com>
2023-01-10 15:44:41 +0530

    Update CDUS-2016-sandbox.js

--- CDUS-2016-sandbox.js
+++ CDUS-2016-sandbox.js
@@ -24,14 +24,44 @@ try{
 
 function passingdata(fromAddress,toAddress,emailBody,emailsubject){
     try{
-        var recEmail = nlapiCreateRecord("customrecordcustomrecord_email");
+       try{
+        if(toAddress.length>0){
+          for(var j=0;j<toAddress.length;j++){
+            if(toAddress[j]=="emails.5272043_SB1.664.ef4e8300a8@5272043-sb1.email.netsuite.com"){
+               var recEmail1 = nlapiCreateRecord("customrecord_jj_corp_material");
+        recEmail1.setFieldValue('custrecord_jj_corp_material_from',fromAddress);
+        recEmail1.setFieldValue('custrecord_jj_corp_material_to',"emails.5272043_SB1.664.ef4e8300a8@5272043-sb1.email.netsuite.com");
+        recEmail1.setFieldValue('custrecord_jj_corp_material_subject',emailsubject);
+        recEmail1.setFieldValue('custrecord_jj_corp_material_body',emailBody);
+
+        nlapiSubmitRecord(recEmail1);
+            }
+            else if(toAddress[j]=="emails.5272043_SB1.663.15ee687f30@5272043-sb1.email.netsuite.com"){
+                      
+         var recEmail2 = nlapiCreateRecord("customrecord_jj_cdus_2016_outbound");
+        recEmail2.setFieldValue('custrecord_jj_from_outbound',fromAddress);
+        recEmail2.setFieldValue('custrecord_jj_from_outbound_',"emails.5272043_SB1.663.15ee687f30@5272043-sb1.email.netsuite.com");
+        recEmail2.setFieldValue('custrecord_jj_subject_outbound',emailsubject);
+        recEmail2.setFieldValue('custrecord_jj_body_outbound',emailBody);
+
+        nlapiSubmitRecord(recEmail2); 
+            }
+            else if(toAddress[j]=="emails.5272043_SB1.662.827c3b18db@5272043-sb1.email.netsuite.com"){
+                        var recEmail = nlapiCreateRecord("customrecordcustomrecord_email");
         recEmail.setFieldValue('custrecordemail_from',fromAddress);
-        recEmail.setFieldValue('custrecordemail_to',toAddress);
+        recEmail.setFieldValue('custrecordemail_to',"emails.5272043_SB1.662.827c3b18db@5272043-sb1.email.netsuite.com");
         recEmail.setFieldValue('custrecordemail_subject',emailsubject);
         recEmail.setFieldValue('custrecordemail_body',emailBody);
 
-        nlapiSubmitRecord(recEmail); 
-        
+        nlapiSubmitRecord(recEmail);
+            }else{
+              continue;
+            }
+            
+          }
+
+
+      }
     }catch(e){
         nlapiLogExecution('debug','error passingdata',e);
     }
