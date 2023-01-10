a6fff94dad5660db154c0ed1ec93ac323771905f
GopikaRaveendran1998 <112853595+GopikaRaveendran1998@users.noreply.github.com>
2023-01-10 15:43:51 +0530

    Update material_cdus_2016.js

--- material_cdus_2016.js
+++ material_cdus_2016.js
@@ -3,20 +3,18 @@ try{
     {
         var fromAddress=email.getFrom().getEmail();
         nlapiLogExecution('debug', 'From Email Address', fromAddress);
-        var toAddress=email.getTo();
-        var arryofTo=[];
-        for(var i in toAddress)
-        {
-            nlapiLogExecution('debug','To Email Address',toAddress[i].getEmail());
-            arryofTo.push(toAddress[i].getEmail());
+      var toAddress = [];
+      var to = email.getTo();
+        for (var index=0; index<to.length; index++) {
+            nlapiLogExecution('debug', 'To Email Address', to[index].getEmail());
+            toAddress.push(to[index].getEmail());
         }
-        nlapiLogExecution('debug','arryofTo',JSON.stringify(arryofTo));
+        nlapiLogExecution('debug', 'To Email Address', JSON.stringify(toAddress));
         var emailBody=email.getTextBody();
         nlapiLogExecution('debug', 'Email Body', emailBody);
         var emailsubject=email.getSubject();
         nlapiLogExecution('debug','Email Subject',emailsubject)
     var funpass=passingdata(fromAddress,toAddress,emailBody,emailsubject);
-        
     }
 }catch(e){
     nlapiLogExecution('debug','error message',e);
@@ -24,14 +22,43 @@ try{
 
 function passingdata(fromAddress,toAddress,emailBody,emailsubject){
     try{
-        var recEmail = nlapiCreateRecord("customrecord_jj_corp_material");
-        recEmail.setFieldValue('custrecord_jj_corp_material_from',fromAddress);
-        recEmail.setFieldValue('custrecord_jj_corp_material_to',toAddress);
-        recEmail.setFieldValue('custrecord_jj_corp_material_subject',emailsubject);
-        recEmail.setFieldValue('custrecord_jj_corp_material_body',emailBody);
+        if(toAddress.length>0){
+          for(var j=0;j<toAddress.length;j++){
+            if(toAddress[j]=="emails.5272043_SB1.664.ef4e8300a8@5272043-sb1.email.netsuite.com"){
+               var recEmail1 = nlapiCreateRecord("customrecord_jj_corp_material");
+        recEmail1.setFieldValue('custrecord_jj_corp_material_from',fromAddress);
+        recEmail1.setFieldValue('custrecord_jj_corp_material_to',"emails.5272043_SB1.664.ef4e8300a8@5272043-sb1.email.netsuite.com");
+        recEmail1.setFieldValue('custrecord_jj_corp_material_subject',emailsubject);
+        recEmail1.setFieldValue('custrecord_jj_corp_material_body',emailBody);
 
-        nlapiSubmitRecord(recEmail); 
-        
+        nlapiSubmitRecord(recEmail1);
+            }
+            else if(toAddress[j]=="emails.5272043_SB1.663.15ee687f30@5272043-sb1.email.netsuite.com"){
+                      
+         var recEmail2 = nlapiCreateRecord("customrecord_jj_cdus_2016_outbound");
+        recEmail2.setFieldValue('custrecord_jj_from_outbound',fromAddress);
+        recEmail2.setFieldValue('custrecord_jj_from_outbound_',"emails.5272043_SB1.663.15ee687f30@5272043-sb1.email.netsuite.com");
+        recEmail2.setFieldValue('custrecord_jj_subject_outbound',emailsubject);
+        recEmail2.setFieldValue('custrecord_jj_body_outbound',emailBody);
+
+        nlapiSubmitRecord(recEmail2); 
+            }
+            else if(toAddress[j]=="emails.5272043_SB1.662.827c3b18db@5272043-sb1.email.netsuite.com"){
+                        var recEmail = nlapiCreateRecord("customrecordcustomrecord_email");
+        recEmail.setFieldValue('custrecordemail_from',fromAddress);
+        recEmail.setFieldValue('custrecordemail_to',"emails.5272043_SB1.662.827c3b18db@5272043-sb1.email.netsuite.com");
+        recEmail.setFieldValue('custrecordemail_subject',emailsubject);
+        recEmail.setFieldValue('custrecordemail_body',emailBody);
+
+        nlapiSubmitRecord(recEmail);
+            }else{
+              continue;
+            }
+            
+          }
+
+
+      }
     }catch(e){
         nlapiLogExecution('debug','error passingdata',e);
     }
